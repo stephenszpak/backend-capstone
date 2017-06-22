b8fb1a09adb97c7869a8555d8887893dac4d0e0e
Stephen Szpak <srszpak@gmail.com>
2017-06-22 16:20:05 -0500

    working and ready to go

---
+++ SilkScreenOrder/app/controllers/customerController.js
@@ -0,0 +1,10 @@
+﻿app.controller("customerController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
+
+    $scope.customerList = [];
+
+    $http.get('api/customer')
+        .then(function (result) {
+            console.log("get customers", result.data)
+            $scope.customerList = result.data;
+        });
+}]);
